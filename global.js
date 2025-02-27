e9b205656bf35f5466cc905fc04d345dc043494d
Effie <52420997+Geoford@users.noreply.github.com>
2025-02-27 09:32:11 +0800

    Update global.js
    ngawngaw sabi ni jambik

--- global.js
+++ global.js
@@ -1,10 +1,12 @@
 var user_location = getCookie("user_location"); 
+let lastValue = $("input[name='switch_country']").val(); // current dropdown value 
 
 
 //next 3 function is the CRUD
 
 //create / update the coockie 
 function setCookie(name, value, days) {
+
   let expires = "";
   if (days) {
     let date = new Date();
@@ -13,10 +15,12 @@ function setCookie(name, value, days) {
   }
   document.cookie =
     name + "=" + encodeURIComponent(value) + expires + "; path=/";
+
 }
 
 //retrieve the cookie
 function getCookie(name) {
+
   let nameEQ = name + "=";
   let cookies = document.cookie.split("; ");
   for (let i = 0; i < cookies.length; i++) {
@@ -25,12 +29,15 @@ function getCookie(name) {
       return decodeURIComponent(c.substring(nameEQ.length));
     }
   }
+
   return null;
 }
 
 //delete the cookie
 function deleteCookie(name) {
+
   document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
 }
 
 //for the popup 
@@ -64,39 +71,42 @@ jQuery(document).ready(function ($) {
     }
   });
 
+
+
+  //for the drop down
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
   }
 
 function updateDropdownValue(location) {
-    if (location) {
-        let formattedLocation = capitalizeFirstLetter(location);
+  if (location === "null") {
+    location = getCookie("user_location"); // Retrieve cookie value if location is null
+  }
+  if (location && location !== "null") {
+      let formattedLocation = capitalizeFirstLetter(location);
 
-        // Update the hidden input field used by MetForm
-        $("input[name='switch_country']").val(formattedLocation.toLowerCase());
-
-        // Update the visible text in the dropdown
-        $(".mf_select__single-value").text(formattedLocation);
-
-        // Update any other related select elements
-        $(".mf-input-select").val(formattedLocation.toLowerCase());
-    }
+      // Update the hidden input field used by MetForm
+      $("input[name='switch_country']").val(formattedLocation.toLowerCase());
+      // Update the visible text in the dropdown
+      $(".mf_select__single-value").text(formattedLocation);
+      // Update any other related select elements
+      $(".mf-input-select").val(formattedLocation.toLowerCase());
+  } 
 }
 
-  function applyStoredLocation() {
-    let userLocation = getCookie("user_location");
-    if (userLocation) {
-      updateDropdownValue(userLocation);
-    }
-  }
+//   function applyStoredLocation() {
+//     let userLocation = getCookie("user_location");
+//     if (userLocation) {
+//       updateDropdownValue(userLocation);
+//     }
+//   }
 
-  let checkMetFormLoaded = setInterval(function () {
-    if ($(".mf_select__single-value").length > 0) {
-        clearInterval(checkMetFormLoaded);
-        applyStoredLocation();
-    }
-}, 500);
-
+//   let checkMetFormLoaded = setInterval(function () {
+//     if ($(".mf_select__single-value").length > 0) {
+//         clearInterval(checkMetFormLoaded);
+//         applyStoredLocation();
+//     }
+// }, 500);
 
   $(document).on("click", ".user-location-setting", function () {
     let selectedLocation = $(this).attr("location");
@@ -106,11 +116,28 @@ function updateDropdownValue(location) {
     }
   });
 
-  let lastValue = $("input[name='switch_country']").val(); // dropdown blyat
-  console.log(lastValue);
+  $(document).on("focus", ".mf-input-select", function () {
+    let selectedValue = $("input[name='switch_country']").val();
+    console.log("Dropdown clicked, maintaining value:", selectedValue);
+
+    if (selectedValue) {
+      setTimeout(() => {
+        updateDropdownValue(selectedValue);
+      }, 100);
+    }
+  });
+
+  let lastValue = $("input[name='switch_country']").val(); // current dropdown value 
+
   
   setInterval(function () {
     let currentValue = $("input[name='switch_country']").val();
+    console.log("currentvalue:" + currentValue);  
+
+    //checker if the input is default again
+    if (currentValue === "null"){
+      currentValue = getCookie("user_location");
+    }
 
     if (currentValue && currentValue !== lastValue) {
         console.log("Detected location change:", currentValue);
@@ -125,6 +152,7 @@ function updateDropdownValue(location) {
         // Update UI without unnecessary re-triggering
         updateDropdownValue(currentValue);
     }
-}, 2000);
+}, 500);
+
 
 });
